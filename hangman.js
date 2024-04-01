742e7f12d3071f34b34cd50bbdd270a354d87162
Ashish-jain0 <164658151+Ashish-jain0@users.noreply.github.com>
2024-04-01 20:25:20 +0530

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -192,18 +192,17 @@ function showGameOverPopup() {
         `;
         document.body.appendChild(popup);
 
-  const playAgainButton = document.getElementById('play-again-button');
-playAgainButton.addEventListener('click', function() {
-    document.body.removeChild(popup);
-    resetGame(); // Call the function to reset the game
-});
-
-
+        const playAgainButton = document.getElementById('play-again-button');
+        playAgainButton.addEventListener('click', function() {
+            document.body.removeChild(popup);
+            resetGame(); // Call the function to reset the game
+        });
 
         document.getElementById('hangman-status').textContent = 'You lost!';
         document.getElementById('hangman-img').src = 'Hanged.jpg'; // Image for losing
     }, 2000); // Display pop-up after 2 seconds
 }
+
 
 function resetGame() {
     chosenCategory = null;
